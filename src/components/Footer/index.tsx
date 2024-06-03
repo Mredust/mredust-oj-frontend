3572d0803accd0f4da54aa1d12b7dacc558e0029
Mredust <3130383647@qq.com>
2024-06-03 13:25:06 +0800

    update:修改DefaultFooter

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,30 +1,34 @@
-import { GithubOutlined } from '@ant-design/icons';
-import { DefaultFooter } from '@ant-design/pro-components';
+import {GithubOutlined} from '@ant-design/icons';
+import {DefaultFooter} from '@ant-design/pro-components';
 import React from 'react';
+import {history} from '@umijs/max';
 
 const Footer: React.FC = () => {
+  const author = 'Mredust';
+  const currentYear = new Date().getFullYear();
+  // 不加载Footer白名单
+  const whiteList: string[] = ['/problemset/*'];
+  const regexPatterns = whiteList.map(item => new RegExp('^' + item.replace('*', '.*') + '$'));
+  const flag = regexPatterns.some(regex => regex.test(history.location.pathname));
+  if (flag) {
+    return null;
+  }
   return (
     <DefaultFooter
       style={{
         background: 'none',
       }}
+      copyright={`${currentYear} ${author}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
           key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          title:
+            (
+              <span style={{margin: '0 8px'}}>
+               <GithubOutlined/> {author}
+            </span>
+            ),
+          href: 'https://github.com/Mredust',
           blankTarget: true,
         },
       ]}
